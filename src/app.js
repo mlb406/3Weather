f98bcd7aa079ddd232b540df7c6508f6a305e307
mlb406 <mlb406@gmail.com>
2015-07-12 16:30:37 +0100

    Nearly finished new UI
    The new user interface for 3Weather v2 is almost complete!

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+//Add desc to new UI
+
 var UI = require('ui');
 var ajax = require('ajax');
 var Vector2 = require('vector2');
@@ -16,9 +18,15 @@ Settings.config(
     var location1 = options.location1;
     var location2 = options.location2;
     var location3 = options.location3;
+    var windUnit = options.units.wind;
+    var tempUnit = options.units.temp;
+    var presUnit = options.units.pressure;
     localStorage.setItem(1, location1);
     localStorage.setItem(2, location2);
     localStorage.setItem(3, location3);
+    localStorage.setItem(4, windUnit);
+    localStorage.setItem(5, tempUnit);
+    localStorage.setItem(6, presUnit);
   }
 
 );
@@ -177,6 +185,9 @@ main.on('click', function(e) {
   var location1 = localStorage.getItem(1);
   var location2 = localStorage.getItem(2);
   var location3 = localStorage.getItem(3);
+  var unitWind = localStorage.getItem(4);
+  var unitTemp = localStorage.getItem(5);
+  var unitPres = localStorage.getItem(6);
   if (e.button === 'up') {
     console.log('up click, ' + location1);
     var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodeURIComponent(location1);
@@ -195,10 +206,35 @@ main.on('click', function(e) {
           function(data, status, request) {
             var kts = Math.round(data.wind.speed*2.23693629*0.868);
             var mph = Math.round(data.wind.speed*2.23693629);
+            var kph = Math.round(data.wind.speed*2.23693629*1.60934);
+            var windUnit;
+            if (unitWind == "kph") {
+              windUnit = kph;
+            } else if (unitWind == "mph") {
+              windUnit = mph;
+            } else if (unitWind == "kts") {
+              windUnit = kts;
+            }
             var deg = Math.round(data.wind.deg);
             var cover = data.clouds.all;
             var hPa = Math.round(data.main.pressure);
-            var c = Math.round(data.main.temp-273);
+            var inHg = data.main.pressure/33.86.toFixed(2);
+            
+            var presUnit;
+            if (unitPres == "hPa") {
+              presUnit = hPa;
+            } else if (unitPres == "inHg") {
+              presUnit = inHg;
+            }
+            var c = Math.round(data.main.temp-273.15);
+            var f = Math.round(data.main.temp-273.15*1.8+32);
+            
+            var tempUnit;
+            if (unitTemp == "c") {
+              tempUnit = c;
+            } else if (unitTemp == "f") {
+              tempUnit = f;
+            }
             var humid = data.main.humidity + '% humidity.';
             var desc1 = data.weather[0].description;
             var cityName = data.name;
@@ -223,14 +259,56 @@ main.on('click', function(e) {
           Menu.show();
             Menu.on('select', function(e) {
               
+              var details;
+              var image = new UI.Image({position: new Vector2(94,60), size: new Vector2(50,50)});
+              var detailWind = new UI.Window({fullscreen: true});
+              var rect = new UI.Rect({position: new Vector2(0,0), size: new Vector2(144,168), backgroundColor: 'white'});
+              detailWind.add(rect);
+              
              if (e.item.title === 'Wind') {
-                 var details = (kts + 'kts, ' + mph + 'mph, from ' + deg + ' degrees.');
+                 details = new UI.Text({
+                   position: new Vector2(0,31),
+                   size: new Vector2(93,60),
+                   font: 'bitham-42-light',
+                   textAlign: 'left',
+                   text: deg + "°\n" + windUnit,
+                   color: 'black'
+                 });
+                 image.image('images/icon_thermo.png');
+               
              } else if (e.item.title === 'Cloud') {
-                 var details = (cover + '%, ' + desc1 + '.');
+                 details = new UI.Text({
+                   position: new Vector2(0,59),
+                   size: new Vector2(93,50),
+                   font: 'bitham-42-light',
+                   textAlign: 'left',
+                   text: cover + "%",
+                   color: 'black'
+                 });
+               image.image('images/icon_speed.png');
+               image.size(new Vector2(51,50));
+               
              } else if (e.item.title === 'Pressure') {
-               var details = (hPa + 'hPa');
+               details = new UI.Text({
+                 position: new Vector2(0,59),
+                 size: new Vector2(93,50),
+                 font: 'bitham-42-light',
+                 textAlign: 'left',
+                 color: 'black',
+                 text: hPa
+               });
+               image.image('images/icon_cloud.png');
+               
              } else if (e.item.title === 'Temperature') {
-               var details = (c + '°c. ' + humid);
+               details = new UI.Text({
+                 position: new Vector2(0,59),
+                 size: new Vector2(93,50),
+                 font: 'bitham-42-light',
+                 textAlign: 'left',
+                 color: 'black',
+                 text: c
+               });
+               image.image('images/icon_cloud.png');
              } else if (e.item.title === 'Summary') {
                  if (kts <= 5) {
                    var windWx = ('light breeze');
@@ -241,14 +319,18 @@ main.on('click', function(e) {
                  } else if (kts >= 35) {
                    var windWx = ('gale force winds');
                  }
-               var details = (c + '°c, ' + windWx + ', ' + desc1 + '.');
+               details = new UI.Text({
+                 position: new Vector2(0,0),
+                 size: new Vector2(144,168),
+                 font: 'gothic-28-bold',
+                 textAlign: 'center',
+                 text: tempUnit + "°" + unitTemp + "\n" + windWx + "\n" + cover + "%\n" + desc1,
+                 color: 'black'
+               });
              }
-             var card = new UI.Card({
-               title: e.item.title,
-               body: details,
-               scrollable: true
-             });
-            card.show();
+              detailWind.add(details);
+              detailWind.add(image);
+              detailWind.show();
             });
           },
       function(error, status, request) {
